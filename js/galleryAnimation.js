416c43eb438879e05160033c39def7e15d067c4c
gracegrace030 <113151468+gracegrace030@users.noreply.github.com>
2023-07-14 20:31:24 -0400

    Gallery animation update

---
+++ js/galleryAnimation.js
@@ -0,0 +1,44 @@
+const carouselToastContainer = document.getElementById("btn-wrap");
+const btnCarousel = document.getElementById("btn-carousel");
+const btnCarouselPaper = document.querySelector("#btn-wrap > .carousel-paper");
+
+const btnCarouselToast = document.getElementById("carousel-toast");
+const carousel = document.getElementById("carousel");
+
+btnCarousel.addEventListener("click", function () {
+  openCarouselToast();
+});
+
+btnCarouselToast.addEventListener("click", function () {
+  closeCarouselToast();
+});
+
+function carouselToastActions() {
+  btnCarousel.classList.toggle("collapse");
+  btnCarousel.classList.toggle("btn-large");
+  btnCarouselPaper.classList.toggle("expand");
+}
+
+function openCarouselToast() {
+  // Bottom part collapse
+  carouselToastActions();
+
+  // Upper part expand
+  setTimeout(function () {
+    btnCarouselToast.classList.toggle("collapse");
+    carouselToastContainer.classList.toggle("moveUp");
+    carousel.classList.toggle("moveUp");
+  }, 200);
+}
+
+function closeCarouselToast() {
+  // Bottom part expand
+  btnCarouselToast.classList.toggle("collapse");
+
+  // Upper part collapse
+  setTimeout(function () {
+    carouselToastActions();
+    carouselToastContainer.classList.toggle("moveUp");
+    carousel.classList.toggle("moveUp");
+  }, 200);
+}
